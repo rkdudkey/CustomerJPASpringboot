fe7ab9d31a63c95659fce8e637a163d3db63d7de
rkdudkey <rattikarn.dudley@gmail.com>
2022-06-15 20:33:35 -0400

    add spring data finder

--- main/java/com/example/dudley/jpa/springdata/customer/repo/CustomerRepository.java
+++ main/java/com/example/dudley/jpa/springdata/customer/repo/CustomerRepository.java
@@ -1,9 +1,20 @@
 package com.example.dudley.jpa.springdata.customer.repo;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import com.example.dudley.jpa.springdata.customer.entity.Customer;
 
+
 public interface CustomerRepository extends CrudRepository<Customer, Long>{
+	List<Customer> findByName(String name);
 	
+	List<Customer> findByEmail(String email);
+	
+
+	List<Customer> findByEmailLike(String email);
+	
+	
+	List<Customer> findByIdIn(List<Object> ids);
 
 }
--- test/java/com/example/dudley/jpa/springdata/customer/CustomerDataApplicationTests.java
+++ test/java/com/example/dudley/jpa/springdata/customer/CustomerDataApplicationTests.java
@@ -3,12 +3,16 @@ package com.example.dudley.jpa.springdata.customer;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.dudley.jpa.springdata.customer.entity.Customer;
 import com.example.dudley.jpa.springdata.customer.repo.CustomerRepository;
+
 
 @SpringBootTest
 class CustomerDataApplicationTests {
@@ -24,8 +28,8 @@ class CustomerDataApplicationTests {
 	void testCustomerCreate() {
 		
 		Customer customer = new Customer();
-		customer.setEmail("ff@bu.com");
-		customer.setName("SOy");
+		customer.setEmail("zz@bu.com");
+		customer.setName("ZOy");
 		
 		customerRepository.save(customer);
 		
@@ -57,6 +61,33 @@ class CustomerDataApplicationTests {
 	@Test
 	void testCount() {
 		System.out.println("total record >>>>>>>>>>>>>>>>>>>>>> " + customerRepository.count());
+	}
+	
+	@Test
+	void findByName() {
+		List<Customer> customer = customerRepository.findByName("SOy");
+		customer.forEach(c-> System.out.println(c.getName()));	
+		
+	}
+	
+	@Test
+	void findByEmail() {
+		List<Customer> customer = customerRepository.findByEmail("zz@bu.com");
+		customer.forEach(c-> System.out.println(c.getName()));	
+		
+	}
+	
+	@Test
+	void findByEmailLike() {
+		List<Customer> customer = customerRepository.findByEmailLike("%zz%");
+		customer.forEach(c-> System.out.println(c.getName() + " " + c.getEmail()));	
+	}
+	
+	@Test
+	void findByIdIn() {
+		long[] a = {2,3};
+		List<Customer> customer = customerRepository.findByIdIn(Arrays.asList(a));
+		customer.forEach(c-> System.out.println(c.getName() + " " + c.getEmail()));	
 	}
 
 }
